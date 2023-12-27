340e4e429c654d35d18aede2e17b51a8c1c8d921
mohammad.aarif <mohammad.aarif@inadev.com>
2023-12-27 16:04:51 +0530

    remve unified totpolog

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 const ConnectDB = async() =>{
     try {
-        const con = await mongoose.connect(process.env.MONGODB_URL,{
-            useUnifiedTopology:true,
-            useNewUrlParser:true
-        })
+        const con = await mongoose.connect(process.env.MONGODB_URL)
         console.log('mongo db connected')
     } catch(error) {
         console.log(`Error: ${error.message}`)
